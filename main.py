89231f9354ce86b0af707868899b443083c805bb
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-04-04 11:18:34 +0500

    Tensorflow GPU has been removed from the project. we have used tensorflow-cpu instead.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model('potatoes.h5')
+MODEL = tf.keras.models.load_model('Model-CPU')
 CLASS_NAMES = ["Potato___Early_blight", "Potato___healthy", "Potato___Late_blight"]
 
 @app.get("/ping")
